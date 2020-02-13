a1cd199c80f978d2a43709e2bb42f3c116333d7f
Peter Duits <peterduits@live.nl>
2020-02-13 13:54:53 +0100

    Added all teams to TeamList

--- src/components/header/NavBar.vue
+++ src/components/header/NavBar.vue
@@ -1,72 +1,63 @@
 <template>
-    <div class="navigation-bar">
-        <v-navigation-drawer v-model="drawer" app>
-            <v-list dense>
-                <v-list-item @click="">
-                    <v-list-item-action>
-                        <v-icon>home</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Home</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                    <v-list-item-action>
-                        <v-icon>contact_mail</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Contact</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+    <div>
+        <div class="navigation-bar">
+            <v-navigation-drawer v-model="drawer" app :disable-resize-watcher="true">
+                <v-list dense>
+                    <v-list-item @click="">
+                        <v-list-item-action>
+                            <v-icon>home</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Home</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-action>
+                            <v-icon>contact_mail</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Contact</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
 
-        <v-app-bar app color="primary" dark>
-            <v-app-bar-nav-icon class="d-block d-sm-none" @click.stop="drawer = !drawer">
-                <v-icon>menu</v-icon>
-            </v-app-bar-nav-icon>
-            <div>
-                <v-img src="@/assets/f1_logo.svg" />
-                <span class="font-weight-bold">Formula One 2020</span>
-            </div>
-            <nav class="navigation d-none d-sm-block">
-                <v-btn
-                    class="navigation__button"
-                    tile
-                    text
-                    large
-                    :to="{ name: 'Home' }"
-                    exact
-                    active-class="navigation__button--active"
-                    >Home</v-btn
-                >
-                <v-btn
-                    class="navigation__button"
-                    tile
-                    text
-                    large
-                    :to="{ name: 'Teams' }"
-                    active-class="navigation__button--active"
-                    >Teams</v-btn
-                >
-            </nav>
-        </v-app-bar>
+            <v-app-bar app color="primary" dark>
+                <v-app-bar-nav-icon class="d-block d-sm-none" @click.stop="drawer = !drawer">
+                    <v-icon>menu</v-icon>
+                </v-app-bar-nav-icon>
+                <div class="logo">
+                    <router-link tag="div" :to="{ name: 'Home' }">
+                        <v-img src="@/assets/f1_logo.svg" />
+                        <span class="font-weight-bold">Formula One 2020</span>
+                    </router-link>
+                </div>
+                <nav class="navigation d-none d-sm-block">
+                    <v-menu content-class="team-dropdown" transition="slide-y-transition" min-width="100%" dark>
+                        <template v-slot:activator="{ on }">
+                            <v-btn v-on="on" class="navigation__button" tile text large>Teams</v-btn>
+                        </template>
+                        <team-list></team-list>
+                    </v-menu>
+                    <v-menu content-class="team-dropdown" transition="slide-y-transition" min-width="100%" dark>
+                        <template v-slot:activator="{ on }">
+                            <v-btn v-on="on" class="navigation__button" tile text large>Drivers</v-btn>
+                        </template>
+                        <team-list></team-list>
+                    </v-menu>
+                </nav>
+            </v-app-bar>
+        </div>
     </div>
 </template>
 
 <script>
+import TeamList from './TeamList';
 export default {
     name: 'NavBar',
-
+    components: { TeamList },
     data: () => ({
         drawer: false,
-        teams: [
-            {
-                name: 'Red Bull',
-                color: '#1E41FF',
-                link: { name: 'RedBull' },
-            },
-        ],
     }),
 };
 </script>
@@ -74,9 +65,22 @@ export default {
 <style lang="scss">
 .navigation-bar {
     .v-toolbar__content {
-        padding: 0 12px;
+        padding: 0 16px;
         max-width: 1785px;
         margin: 0 auto;
+    }
+
+    .logo {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100%;
+        margin-right: 50px;
+
+        &:hover {
+            cursor: pointer;
+            border-bottom: 5px solid #15151e;
+        }
     }
 
     .navigation {
@@ -87,17 +91,35 @@ export default {
             height: 100% !important;
 
             &:hover {
+                cursor: pointer;
+                border-bottom: 5px solid #15151e;
+            }
+
+            &:focus {
                 background: #15151e;
+
+                &::before {
+                    opacity: 0 !important;
+                }
             }
 
             &--active {
-                background-color: #15151e;
+                border-bottom: 5px solid #15151e;
 
                 &::before {
                     opacity: 0 !important;
                 }
             }
         }
+    }
+}
+.team-dropdown {
+    top: 56px !important;
+    border-radius: 0 !important;
+    background-color: #15151e;
+
+    @media screen and (min-width: 960px) {
+        top: 64px !important;
     }
 }
 </style>
---
+++ src/components/header/TeamList.vue
@@ -0,0 +1,185 @@
+<template>
+    <div class="container team-list">
+        <div class="team-list__nav">
+            <v-btn :to="{ name: 'Teams' }">Alle Teams</v-btn>
+        </div>
+        <div class="teams">
+            <template v-for="(team, i) in teams">
+                <div class="team-container col-12 col-sm-6 col-md-4 col-lg-3">
+                    <router-link class="team" tag="div" :to="team.link" :key="i" :style="{ color: team.color }">
+                        <div class="team-header">
+                            <span class="team-header__name">{{ team.name }}</span>
+                            <v-icon>keyboard_arrow_right</v-icon>
+                        </div>
+                        <div class="team-content">
+                            <div class="team-content__stripe" :style="{ backgroundColor: team.color }"></div>
+                            <img class="team-content__image" :src="team.image.url" :alt="team.image.alt" />
+                        </div>
+                    </router-link>
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TeamList',
+
+    data: () => ({
+        teams: [
+            {
+                name: 'Alfa Romeo',
+                color: '#9B0000',
+                image: {
+                    url: require('@/assets/teams/alfaromeo/car.png'),
+                    alt: 'Afla Romeo Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Haas',
+                color: '#bd9e57',
+                image: {
+                    url: require('@/assets/teams/haas/car.png'),
+                    alt: 'Haas Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Racing Point',
+                color: '#F596C8',
+                image: {
+                    url: require('@/assets/teams/racingpoint/car.png'),
+                    alt: 'Racing Point Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Williams',
+                color: '#FFFFFF',
+                image: {
+                    url: require('@/assets/teams/williams/car.png'),
+                    alt: 'Williams Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Alpha Tauri',
+                color: '#469BFF',
+                image: {
+                    url: require('@/assets/teams/alphatauri/car.png'),
+                    alt: 'Alpha Tauri Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'McLaren',
+                color: '#FF8700',
+                image: {
+                    url: require('@/assets/teams/mclaren/car.png'),
+                    alt: 'McLaren Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Red Bull',
+                color: '#1E41FF',
+                image: {
+                    url: require('@/assets/teams/redbull/car.png'),
+                    alt: 'Red Bull Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Ferrari',
+                color: '#DC0000',
+                image: {
+                    url: require('@/assets/teams/ferrari/car.png'),
+                    alt: 'Ferrari Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Mercedes',
+                color: '#00D2BE',
+                image: {
+                    url: require('@/assets/teams/mercedes/car.png'),
+                    alt: 'Mercedes Car',
+                },
+                link: { name: 'Home' },
+            },
+            {
+                name: 'Renault',
+                color: '#FFF500',
+                image: {
+                    url: require('@/assets/teams/renault/car.png'),
+                    alt: 'Renault Car',
+                },
+                link: { name: 'Home' },
+            },
+        ],
+    }),
+};
+</script>
+
+<style lang="scss">
+.team-list {
+    width: 100vw;
+
+    &__nav {
+        display: flex;
+        justify-content: space-between;
+        margin: 50px 0;
+    }
+
+    .teams {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .team-container {
+        width: 25%;
+        padding: 0 40px 40px 0;
+
+        .team {
+            padding: 0 10px 10px 0;
+            border-radius: 0 0 10px 0;
+            border-bottom: 2px solid #666666;
+            border-right: 2px solid #666666;
+            transition: all 0.3s ease-in-out;
+
+            &:hover {
+                cursor: pointer;
+                border-color: currentColor;
+
+                .team-content__stripe {
+                    width: 100px;
+                    transition: all 0.3s ease-in-out;
+                }
+            }
+
+            .team-header {
+                display: flex;
+                justify-content: space-between;
+                color: #ffffff;
+            }
+
+            .team-content {
+                display: flex;
+
+                &__stripe {
+                    height: 20px;
+                    width: 40px;
+                    margin: 20px -20px 0 0;
+                    align-self: center;
+                    transition: all 0.3s ease-in-out;
+                }
+                &__image {
+                    width: 70%;
+                }
+            }
+        }
+    }
+}
+</style>
